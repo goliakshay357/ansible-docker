08877a93faed86f895dde9d24b1bae2011b725c5
goliakshay357 <goliakshay357@gmail.com>
2021-04-27 16:40:47 +0530

    changed 3

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -5,4 +5,6 @@ echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
 echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
 echo "Entering the ansible using ansible-playbook"
+echo "$SSH_PASSWORD"
+
 ansible-playbook ansible/playbook.yml --user ubuntu
