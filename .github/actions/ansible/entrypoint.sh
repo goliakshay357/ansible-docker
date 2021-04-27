b8a31a627ea51d0d679579325187b7d60ea43662
goliakshay357 <goliakshay357@gmail.com>
2021-04-27 17:18:09 +0530

    added secrets

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 echo "Ansible Entrypoint"
 
-echo "ansible_ssh_pass=goli" >> /hosts
-echo "ansible_become_pass=goli" >> /hosts
+echo "ansible_ssh_pass=$SSH_PASSWORD" >> /hosts
+echo "ansible_become_pass=$SSH_PASSWORD" >> /hosts
 
 echo "Entering the ansible using ansible-playbook"
-echo "$SSH_PASSWORD"
 
 ansible-playbook ansible/playbook.yml --user ubuntu
